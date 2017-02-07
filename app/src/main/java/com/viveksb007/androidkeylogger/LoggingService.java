49ae5bf12c6774f0c0e3124ce6412cbe57afd4e0
Vivek Singh Bhadauria <viveksbhadauria007@gmail.com>
2017-02-07 22:53:47 +0530

    initial commit

---
+++ app/src/main/java/com/viveksb007/androidkeylogger/LoggingService.java
@@ -0,0 +1,22 @@
+package com.viveksb007.androidkeylogger;
+
+import android.accessibilityservice.AccessibilityService;
+import android.view.accessibility.AccessibilityEvent;
+
+/**
+ * Created by viveksb007 on 7/2/17.
+ */
+
+public class LoggingService extends AccessibilityService {
+
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent accessibilityEvent) {
+
+    }
+
+    @Override
+    public void onInterrupt() {
+
+    }
+
+}
