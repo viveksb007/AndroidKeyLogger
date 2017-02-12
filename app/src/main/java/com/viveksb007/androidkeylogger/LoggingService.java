1e0a044c1af347a22510b4bb622e4549cbe72234
Vivek Singh Bhadauria <viveksbhadauria007@gmail.com>
2017-02-13 00:04:29 +0530

    zz

--- app/src/main/java/com/viveksb007/androidkeylogger/LoggingService.java
+++ app/src/main/java/com/viveksb007/androidkeylogger/LoggingService.java
@@ -1,6 +1,9 @@
 package com.viveksb007.androidkeylogger;
 
 import android.accessibilityservice.AccessibilityService;
+import android.accessibilityservice.AccessibilityServiceInfo;
+import android.os.Build;
+import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 
 /**
@@ -9,14 +12,38 @@ import android.view.accessibility.AccessibilityEvent;
 
 public class LoggingService extends AccessibilityService {
 
+    private static final String TAG = "LOGGING_SERVICE";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.v(TAG, "created");
+    }
+
     @Override
     public void onAccessibilityEvent(AccessibilityEvent accessibilityEvent) {
-
+        Log.v(TAG, "i am fired");
     }
 
     @Override
     public void onInterrupt() {
-
+        Log.v(TAG, "onInterrupt");
     }
 
+    private String getEventText(AccessibilityEvent event) {
+        StringBuilder sb = new StringBuilder();
+        for (CharSequence s : event.getText()) {
+            sb.append(s);
+        }
+        return sb.toString();
+    }
+
+    @Override
+    protected void onServiceConnected() {
+        super.onServiceConnected();
+        Log.v(TAG, "onServiceConnected");
+        AccessibilityServiceInfo info = new AccessibilityServiceInfo();
+        info.feedbackType = AccessibilityServiceInfo.FEEDBACK_GENERIC;
+        setServiceInfo(info);
+    }
 }
